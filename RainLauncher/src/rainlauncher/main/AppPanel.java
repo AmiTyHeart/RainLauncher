e915a9c45d40fe21faa5dd794a28b876e0f325e6
Defwentz <dfwentzz@gmail.com>
2014-10-09 19:59:53 +0800

    icon became transparent when touched, X

--- RainLauncher/src/rainlauncher/main/AppPanel.java
+++ RainLauncher/src/rainlauncher/main/AppPanel.java
@@ -7,7 +7,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,8 +45,14 @@ public class AppPanel extends ViewGroup {
 			final String cls = info.activityInfo.name;  
 			Drawable d = info.loadIcon(pm);
 			View view = LayoutInflater.from(mContext).inflate(R.layout.app_item, null);
-			((ImageView)view.findViewById(R.id.imageView)).setImageDrawable(d);
-			((ImageView)view.findViewById(R.id.imageView)).setOnClickListener(
+			ImageView iv = (ImageView)view.findViewById(R.id.imageView);
+			iv.setImageDrawable(d);
+
+			// 制出透明图标, 放到statelist中
+	        StateListDrawable sld = createSLD(mContext, d);
+	        iv.setImageDrawable(sld);
+			
+			iv.setOnClickListener(
 					new OnClickListener(){
 						@Override
 						public void onClick(View arg0) {							
@@ -56,6 +68,53 @@ public class AppPanel extends ViewGroup {
 			
 		}
 	}
+
+	
+	public Drawable createDrawable(Drawable d, Paint p) {
+		
+        BitmapDrawable bd = (BitmapDrawable)d;
+        Bitmap b = bd.getBitmap();
+        Bitmap bitmap = Bitmap.createBitmap(bd.getIntrinsicWidth(),
+                bd.getIntrinsicHeight(), Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawBitmap(b, 0, 0, p); // 关键代码，使用新的Paint画原图，
+
+        return new BitmapDrawable(bitmap);
+    }
+
+
+    /** 设置Selector。 本次只增加点击变暗的效果，注释的代码为更多的效果*/ 
+    public StateListDrawable createSLD(Context context, Drawable drawable) {
+        StateListDrawable bg = new StateListDrawable();
+        Paint p = new Paint();
+        p.setColor(0x40222222); //Paint ARGB色值，A = 0x40 不透明。RGB222222 暗色
+
+
+        Drawable normal = drawable;
+        Drawable pressed = createDrawable(drawable, p);
+        // p = new Paint();
+        // p.setColor(0x8000FF00);
+        // Drawable focused = createDrawable(drawable, p);
+        // p = new Paint();
+        // p.setColor(0x800000FF);
+        // Drawable unable = createDrawable(drawable, p);
+        // View.PRESSED_ENABLED_STATE_SET
+        bg.addState(new int[] { android.R.attr.state_pressed,
+                android.R.attr.state_enabled }, pressed);
+        // View.ENABLED_FOCUSED_STATE_SET
+        // bg.addState(new int[] { android.R.attr.state_enabled,
+        // android.R.attr.state_focused }, focused);
+        // View.ENABLED_STATE_SET
+        bg.addState(new int[] { android.R.attr.state_enabled }, normal);
+        // View.FOCUSED_STATE_SET
+        // bg.addState(new int[] { android.R.attr.state_focused }, focused);
+        // // View.WINDOW_FOCUSED_STATE_SET
+        // bg.addState(new int[] { android.R.attr.state_window_focused },
+        // unable);
+        // View.EMPTY_STATE_SET
+        bg.addState(new int[] {}, normal);
+        return bg;
+    }
 	
 	@Override
 	protected void onLayout(boolean change, int l, int t, int r, int b) {
