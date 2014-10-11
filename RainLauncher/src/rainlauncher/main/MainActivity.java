9b6bb59fce3a133cee7908f5c8673ca0202bc069
StoriesUntold <997989676@qq.com>
2014-10-11 14:43:49 +0800

    change the menu view,but the layout isn't good-looking,C

--- RainLauncher/src/rainlauncher/main/MainActivity.java
+++ RainLauncher/src/rainlauncher/main/MainActivity.java
@@ -4,24 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.ComponentName;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -33,8 +22,7 @@ public class MainActivity extends Activity {
 	private List<ResolveInfo> mList;
 	private PackageManager mPackageManager;
 	private ScrollPanel layout;
-	
-	private final int MENU_FIRST = 0;
+	Dialog menu;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +45,8 @@ public class MainActivity extends Activity {
 		//init();
 		AppPanel panel = new AppPanel(mCtx, mPackageManager, mList);
 		layout.addView(panel);
+		
+		menu = new MenuDialog(this, R.style.Dialog_Fullscreen);
 	}
 	
 /*
@@ -130,57 +120,15 @@ public class MainActivity extends Activity {
             return position;           
         }       
     }
-*/
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
-    	menu.add(Menu.NONE, MENU_FIRST, 1, "设置壁纸")
-    		.setIcon(android.R.drawable.btn_star);
-    	menu.add(Menu.NONE, MENU_FIRST + 1, 2, "桌面设置")
-    		.setIcon(android.R.drawable.btn_star);
-    	menu.add(Menu.NONE, MENU_FIRST + 2, 3, "设置")
-			.setIcon(android.R.drawable.ic_menu_set_as);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch(id) {
-        case MENU_FIRST:
-        	onSetWallpaper();
-            return true;
-        case MENU_FIRST + 1:
-        	return true;
-        case MENU_FIRST + 2:
-        	ComponentName componet = new ComponentName("com.android.settings",
-        			"com.android.settings.Settings");                          
-        	Intent i = new Intent();              
-        	i.setComponent(componet);  
-        	startActivity(i); 
-        	return true;
-        } 
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void onSetWallpaper() {                           
-        //生成一个设置壁纸的请求                
-        final Intent pickWallpaper = new Intent(Intent.ACTION_SET_WALLPAPER);                   
-        Intent chooser = Intent.createChooser(pickWallpaper,"选择壁纸");                   
-        //发送设置壁纸的请求                   
-        startActivity(chooser);       
-    }  
-    
+*/       
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
 			Toast.makeText(this, "哈哈，回不去了！", Toast.LENGTH_SHORT).show();
 			return false;
-		} else {
-			return super.onKeyDown(keyCode, event);
+		}else if(keyCode == KeyEvent.KEYCODE_MENU){
+			menu.show();
+			return false;
 		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
---
+++ RainLauncher/src/rainlauncher/main/MenuDialog.java
@@ -0,0 +1,77 @@
+package rainlauncher.main;
+
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MenuDialog extends Dialog{
+
+	Context context;
+    ImageButton wallpaper, set, selfset;
+    public MenuDialog(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+        this.context = context;
+    }
+    public MenuDialog(Context context, int theme){
+        super(context, theme);
+        this.context = context;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.menu);
+
+        wallpaper = (ImageButton) findViewById(R.id.wallpaper);
+        set = (ImageButton) findViewById(R.id.set);
+        selfset = (ImageButton) findViewById(R.id.selfset);
+
+        wallpaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            	onSetWallpaper();
+            }
+        });
+        
+        set.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				ComponentName componet = new ComponentName("com.android.settings",
+	        			"com.android.settings.Settings");                          
+	        	Intent i = new Intent();              
+	        	i.setComponent(componet);  
+	        	context.startActivity(i); 
+			}
+        });
+        
+        selfset.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(context, "selfset", Toast.LENGTH_SHORT).show();
+			}
+        });
+    }
+    
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	if(keyCode == KeyEvent.KEYCODE_MENU){
+    		dismiss();
+    		return false;
+    	}
+    	return super.onKeyDown(keyCode, event);
+    }
+    
+    public void onSetWallpaper() {                           
+        //生成一个设置壁纸的请求                
+        final Intent pickWallpaper = new Intent(Intent.ACTION_SET_WALLPAPER);                   
+        Intent chooser = Intent.createChooser(pickWallpaper,"选择壁纸");                   
+        //发送设置壁纸的请求                   
+        context.startActivity(chooser);       
+    }
+}
